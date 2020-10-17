b010ec0614bde40b0403ca599a536e08c723cfc7
Sascha Mann <git@mail.saschamann.eu>
2020-10-17 12:45:54 +0200

    Handle removal of tests better

--- bin/check-immutability.py
+++ bin/check-immutability.py
@@ -17,18 +17,33 @@ new = json.loads(subprocess.run([f"jq -r '[.. | objects | select(.uuid != null)]
 new = {c['uuid']: c for c in new}
 
 fails = set()
+deleted = set()
 
 # Iterate through old cases as only those could potentially be mutated
 for case in old:
+    uuid = case['uuid']
+
+    # Check if the case has been deleted
+    if uuid not in new:
+        deleted.add(uuid)
+        continue
+
     for k in immutable_keys:
-        if case[k] != new[case['uuid']][k]:
-            fails.add(case['uuid'])
+        if case[k] != new[uuid][k]:
+            fails.add(uuid)
             break
 
-if len(fails) == 0:
+if len(fails) == 0 and len(deleted) == 0:
     exit(0)
 
-print('The following tests contain illegal mutations:')
-for f in fails:
-    print(f" - {f} ({new[f]['description']})")
+if len(fails) > 0:
+    print('The following tests contain illegal mutations:')
+    for f in fails:
+        print(f" - {f} ({new[f]['description']})")
+
+if len(deleted) > 0:
+    print('The following tests have been deleted illegally:')
+    for d in deleted:
+        print(f" - {d}")
+
 exit(1)
