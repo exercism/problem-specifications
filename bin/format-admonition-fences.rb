16b20e6b1d8f0543ad2ea334a22a919282d37d36
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-20 16:41:10 +0200

    Minor rename

--- bin/format-admonition-fences.rb
+++ bin/format-admonition-fences.rb
@@ -1,4 +1,4 @@
-BACKTICK_MATCH_REGEX = /(?<start>`{3,})(?<type>exercism\/[a-z]+)(?<admonition>.*?)(?<end>\k<start>)/.freeze
+BACKTICK_MATCH_REGEX = /(?<fence>`{3,})(?<type>exercism\/[a-z]+)(?<admonition>.*?)(?<end>\k<fence>)/.freeze
 TILDE_REPLACE = '~~~~\k<type>\k<admonition>~~~~'.freeze
 
 Dir.glob('exercises/**/*.md') do |filepath|
