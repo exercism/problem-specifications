64f3654fe5fc69d3a34e16171e25c1ce19df5bdd
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-20 16:00:28 +0200

    Add script to format admonitions

---
+++ bin/format-admonitions.rb
@@ -0,0 +1,8 @@
+BACKTICK_MATCH_REGEX = /(?<start>`{3,})(?<type>exercism\/[a-z]+)(?<admonition>[\s\S]*?)(?<end>\k<start>)/.freeze
+TILDE_REPLACE = '~~~~\k<type>\k<admonition>~~~~'.freeze
+
+Dir.glob('exercises/**/*.md') do |filepath|
+  contents = File.read(filepath)
+  contents.gsub!(BACKTICK_MATCH_REGEX, TILDE_REPLACE)
+  File.write(filepath, contents)
+end
